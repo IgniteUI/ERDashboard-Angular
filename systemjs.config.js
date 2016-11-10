6b30a8a63dcede4a64e012c5f1e47b7a324c6e17
Deyan Kamburov <dkamburov@infragistics.com>
2016-11-10 20:11:15 +0200

    Configure gh-pages

--- systemjs.config.js
+++ systemjs.config.js
@@ -6,7 +6,7 @@
   System.config({
     paths: {
       // paths serve as alias
-      'npm:': 'libs/'
+      'npm:': 'https://unpkg.com/'
     },
     // map tells the System loader where to look for things
     map: {
@@ -24,13 +24,13 @@
       // other libraries
       'rxjs':                      'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
-      'igniteui-angular2': 'npm:igniteui'
+      'igniteui-angular2': 'npm:igniteui-angular2'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
       app: {
-        main: './main.js',
-        defaultExtension: 'js'
+        main: './main.ts',
+        defaultExtension: 'ts'
       },
       rxjs: {
         defaultExtension: 'js'
@@ -40,9 +40,11 @@
         defaultExtension: 'js'
       },
       'igniteui-angular2': {
-          main: 'index.js',
-          defaultExtension: 'js'
+          main: './igniteui.angular2.ts',
+          defaultExtension: 'ts'
       }
-    }
+    },
+    transpiler: 'typescript',
+    typescriptOptions: {emitDecoratorMetadata: true}
   });
 })(this);
